d7058e75520f3604cd34d0efd0a2399263febfac
HarrisonBorges <harrisonborges@outlook.com.br>
2018-08-29 18:46:50 -0300

    nulo e vazio

--- src/main/java/mediana/Mediana.java
+++ src/main/java/mediana/Mediana.java
@@ -1,17 +1,34 @@
 package mediana;
 
+import java.util.Arrays;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Harrison
  */
 class Mediana {
-public double getMediana(double[] v){
-    return 3.0;
-}    
+
+    public double getMediana(double[] v) {
+        return 3.0;
+    }
+
+    public double estatisticaNuloVazio(double[] vetor) {
+        if (vetor == null) {
+            throw new NullPointerException();
+        }
+        if (vetor.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        Arrays.sort(vetor);
+        if (vetor.length % 2 != 0) {
+            return vetor[vetor.length / 2];
+        }
+        return (vetor[(vetor.length / 2) - 1] + vetor[vetor.length / 2]) / 2;
+    }
+
 }
--- src/test/java/mediana/MedianaTest.java
+++ src/test/java/mediana/MedianaTest.java
@@ -13,13 +13,43 @@ import static org.junit.Assert.*;
  * @author Harrison
  */
 public class MedianaTest {
-    
+
+    Mediana estatistica;
+
+    public MedianaTest() {
+        estatistica = new Mediana();
+    }
+
+    //(i) Encontre a mediana dos números.
     @Test
     public void testeVetorOrdenadoImpar() {
-        double[] v = {1,2,3,4,5};
+        double[] v = {1, 2, 3, 4, 5};
         Mediana m = new Mediana();
         double res = m.getMediana(v);
         assertEquals(3, res, 0.01);
-  
     }
+
+    @Test(expected = NullPointerException.class)
+    public void medianaTestNulo() {
+        assertEquals(0.0, estatistica.estatisticaNuloVazio(null), 0.01);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void medianaVaziaTest() {
+        double[] vetor = {};
+        assertEquals(0.0, estatistica.estatisticaNuloVazio(vetor), 0.01);
+    }
+
+    @Test
+    public void medianaParQuantidadeTest() {
+        double[] vetor = {3, 5, 6, 8, 9, 12, 15};
+        assertEquals(8.0, estatistica.estatisticaNuloVazio(vetor), 0.01);
+    }
+
+    @Test
+    public void medianaImparQuantidadeTest() {
+        double[] vetor = {3, 5, 6, 8, 9, 12, 15, 16};
+        assertEquals(8.5, estatistica.estatisticaNuloVazio(vetor), 0.01);
+    }
+
 }
